2b89c45732dd0ff77046bf74d66c931fcc802ad3
shamreennus <shammi.reena@gmail.com>
2023-03-29 18:42:59 -0400

    Initial commit UI

---
+++ UI/HTML_CSS_JSP/header.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
---
+++ UI/HTML_CSS_JSP/script.js
@@ -0,0 +1,9 @@
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // You can add your login logic here
+});
